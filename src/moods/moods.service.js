1d7f64cbd8715c4133c16dc992348aafb7722897
miaslls <97785569+miaslls@users.noreply.github.com>
2022-07-16 06:56:42 -0300

    chore: sort allMoods

--- src/moods/moods.service.js
+++ src/moods/moods.service.js
@@ -2,4 +2,4 @@
 
 import Mood from './Mood.js';
 
-export const getAllMoodsService = () => Mood.find().populate('user');
+export const getAllMoodsService = () => Mood.find().sort({ _id: -1 }).populate('user');
