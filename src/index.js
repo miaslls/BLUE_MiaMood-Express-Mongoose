e9b050377a861ba72668e2e227f13ffcfff791df
miaslls <97785569+miaslls@users.noreply.github.com>
2022-07-16 01:21:36 -0300

    chore: initial index config

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 'use strict';
+
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+// import databaseConnection from './database/dbConnection.js';
+// import authRoute from './auth/auth.route.js';
+// import swaggerRoute from './swagger/swagger.routes.js';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+// databaseConnection();
+
+app.use(cors());
+app.use(express.json());
+
+// app.use('/users', userRoutes);
+// app.use('/auth', authRoute);
+// app.use('/api-docs', swaggerRoute);
+
+app.listen(port, () => {
+  console.log(`server running @ port ${port}`);
+});
