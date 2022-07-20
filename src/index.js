48e03a556b430b4095a19bb9d252e9cb72e2cb4f
miaslls <97785569+miaslls@users.noreply.github.com>
2022-07-20 20:01:00 -0300

    refactor: dotenv config

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@
 
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
+import 'dotenv/config';
 import databaseConnection from './database/dbConnection.js';
 import authRoute from './auth/auth.route.js';
 import usersRoute from './users/users.route.js';
 import moodsRoute from './moods/moods.route.js';
 import swaggerRoute from './swagger/swagger.route.js';
-
-dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3001;
