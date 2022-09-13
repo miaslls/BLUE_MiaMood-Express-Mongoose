d600c48fd0486346ce3cc88407fe90a65aca8b9d
miaslls <97785569+miaslls@users.noreply.github.com>
2022-09-13 07:51:29 -0300

    is this better? idk.. time will tell

--- src/lists/List.js
+++ src/lists/List.js
@@ -4,6 +4,7 @@ const ListSchema = new mongoose.Schema(
   {
     name: { type: String, required: true, unique: true },
     icon: { type: String, required: true, unique: true },
+    entries: [{ type: mongoose.Schema.Types.ObjectId, ref: 'ListEntry' }],
   },
   { versionKey: false },
 );
--- src/lists/ListEntry.js
+++ src/lists/ListEntry.js
@@ -2,15 +2,10 @@ import mongoose from 'mongoose';
 
 const ListEntrySchema = new mongoose.Schema(
   {
-    list: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'List',
-      required: true,
-    },
+    list: { type: mongoose.Schema.Types.ObjectId, ref: 'List', required: true },
     text: { type: String, required: true },
-    dateTime: { type: String, required: true },
-    starred: { type: Boolean, required: true },
-    completed: { type: Boolean, required: true },
+    starred: { type: Boolean },
+    completed: { type: Boolean },
   },
   { versionKey: false },
 );
