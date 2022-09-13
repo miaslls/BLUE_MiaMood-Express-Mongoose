f8242c31dd7468d98dd1c9e9534f5775e9196433
miaslls <97785569+miaslls@users.noreply.github.com>
2022-09-13 06:52:06 -0300

    remove unused

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@
 import express from 'express';
 import cors from 'cors';
 import databaseConnection from './database/dbConnection.js';
-import authRoute from './auth/auth.route.js';
-import usersRoute from './users/users.route.js';
+
+// import authRoute from './auth/auth.route.js';
+// import usersRoute from './users/users.route.js';
+// import swaggerRoute from './swagger/swagger.route.js';
+
 import moodsRoute from './moods/moods.route.js';
-import swaggerRoute from './swagger/swagger.route.js';
 import 'dotenv/config';
 
 const app = express();
@@ -17,10 +19,11 @@ databaseConnection();
 app.use(cors());
 app.use(express.json());
 
-app.use('/auth', authRoute);
-app.use('/users', usersRoute);
+// app.use('/auth', authRoute);
+// app.use('/users', usersRoute);
+// app.use('/api-docs', swaggerRoute);
+
 app.use('/moods', moodsRoute);
-app.use('/api-docs', swaggerRoute);
 
 app.listen(port, () => {
   console.log(`server running @ port ${port}`);
