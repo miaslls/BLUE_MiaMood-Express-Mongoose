f9216e9fdc4e78bbe1ae8f6af2dbd0d201235776
miaslls <97785569+miaslls@users.noreply.github.com>
2022-09-13 09:40:11 -0300

    remove unused

--- src/moods/moods.route.js
+++ src/moods/moods.route.js
@@ -13,16 +13,17 @@ import {
   updateMoodController,
   deleteMoodController,
 } from './moods.controller.js';
+
 import { validMoodId, validMoodBody } from './moods.middleware.js';
 // import authMiddleware from '../auth/auth.middleware.js';
 
 router.post('/', validMoodBody, createMoodController);
 router.get('/', getAllMoodsController);
-router.get('/date', getMoodsByDateController);
-router.get('/date/today', getTodayMoodsController);
-router.get('/search', searchMoodsController);
-router.get('/id/:id', validMoodId, getMoodByIdController);
 router.put('/update/:id', validMoodId, validMoodBody, updateMoodController);
 router.delete('/delete/:id', validMoodId, deleteMoodController);
+// router.get('/date', getMoodsByDateController);
+// router.get('/date/today', getTodayMoodsController);
+// router.get('/search', searchMoodsController);
+// router.get('/id/:id', validMoodId, getMoodByIdController);
 
 export default router;
