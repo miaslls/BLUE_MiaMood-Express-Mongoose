5d77795a09f54b04fe34b63a41f6e242048b9bc9
miaslls <97785569+miaslls@users.noreply.github.com>
2022-09-30 23:19:10 -0300

    ListCategory

---
+++ src/lists/listCategories/ListCategory.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const ListCategorySchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, unique: true },
+    icon: { type: String, required: true, unique: true },
+    lists: [{ type: mongoose.Schema.Types.ObjectId, ref: 'List' }],
+  },
+  { versionKey: false },
+);
+
+const ListCategory = mongoose.model('ListCategory', ListCategorySchema, 'list-categories');
+
+export default ListCategory;
