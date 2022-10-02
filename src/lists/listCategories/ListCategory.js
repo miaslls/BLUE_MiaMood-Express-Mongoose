9ad93ecb87cf53e1ef2ba91fb7de3124914a3eaa
miaslls <97785569+miaslls@users.noreply.github.com>
2022-10-02 01:49:18 -0300

    remove unnecessary code

--- src/lists/listCategories/ListCategory.js
+++ src/lists/listCategories/ListCategory.js
@@ -4,8 +4,6 @@ const ListCategorySchema = new mongoose.Schema(
   {
     name: { type: String, required: true, unique: true },
     icon: { type: String, required: true, unique: true },
-    // 👁‍🗨 is this 🔻 really necessary?
-    // lists: [{ type: mongoose.Schema.Types.ObjectId, ref: 'List' }],
   },
   { versionKey: false },
 );
