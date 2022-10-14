315c818b2b53143b5ff79bf4210abb296b46b0fe
miaslls <97785569+miaslls@users.noreply.github.com>
2022-10-14 18:48:06 -0300

    enable /auth

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import databaseConnection from './database/dbConnection.js';
 import 'dotenv/config';
 
-// import authRoute from './auth/auth.route.js';
+import authRoute from './auth/auth.route.js';
 // import usersRoute from './users/users.route.js';
 // import swaggerRoute from './swagger/swagger.route.js';
 
@@ -20,7 +20,7 @@ databaseConnection();
 app.use(cors());
 app.use(express.json());
 
-// app.use('/auth', authRoute);
+app.use('/auth', authRoute);
 // app.use('/users', usersRoute);
 // app.use('/api-docs', swaggerRoute);
 
