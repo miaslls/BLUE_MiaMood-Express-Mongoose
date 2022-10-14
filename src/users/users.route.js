07e67233170f8819764ceef8aad5df296cb14285
miaslls <97785569+miaslls@users.noreply.github.com>
2022-10-14 19:37:22 -0300

    minor

--- src/users/users.route.js
+++ src/users/users.route.js
@@ -8,10 +8,10 @@ import {
   getAllUsersController,
   deleteUserController,
 } from './users.controller.js';
-import authMiddleware from '../auth/auth.middleware.js';
+// import authMiddleware from '../auth/auth.middleware.js';
 
 router.post('/', createUserController);
-router.get('/', authMiddleware, getAllUsersController);
+router.get('/', getAllUsersController);
 router.delete('/:id', deleteUserController);
 
 export default router;
